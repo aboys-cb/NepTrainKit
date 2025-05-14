e364a4f5c982243dd760d27aca7fe007817d50ea
SuperBing <1747193328@qq.com>
2025-05-14 21:36:42 +0800

    pytest

--- tests/test_base.py
+++ tests/test_base.py
@@ -1,34 +1,34 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-import unittest
+import pytest
 import numpy as np
 from pathlib import Path
 import os
 from NepTrainKit.core.io.base import NepPlotData, StructureData
 from NepTrainKit.core.structure import Structure
-class TestBaseClasses(unittest.TestCase):
-    def setUp(self):
-        self.test_data = np.random.rand(10, 6)
-        self.test_indices = np.arange(10)
-        self.test_dir = Path(__file__).parent
 
-    def test_nep_plot_data(self):
-        """测试NepPlotData基本功能"""
-        data = NepPlotData(self.test_data )
-        self.assertEqual(data.num, 10)
-        self.assertEqual(data.now_data.shape, (10, 6))
-        data.remove([0,1])
-        self.assertEqual(data.now_data.shape, (8, 6))
-        self.assertEqual(data.remove_data.shape, (2, 6))
-        data.revoke()
-        self.assertEqual(data.now_data.shape, (10, 6))
+@pytest.fixture
+def test_setup():
+    test_data = np.random.rand(10, 6)
+    test_indices = np.arange(10)
+    test_dir = Path(__file__).parent
+    return test_data, test_indices, test_dir
 
-    def test_structure_data(self):
-        """测试StructureData基本功能"""
+def test_nep_plot_data(test_setup):
+    """测试NepPlotData基本功能"""
+    test_data, _, _ = test_setup
+    data = NepPlotData(test_data)
+    assert data.num == 10
+    assert data.now_data.shape == (10, 6)
+    data.remove([0, 1])
+    assert data.now_data.shape == (8, 6)
+    assert data.remove_data.shape == (2, 6)
+    data.revoke()
+    assert data.now_data.shape == (10, 6)
 
-        structures =Structure.read_multiple(os.path.join(self.test_dir,"data/nep/train.xyz"))
-        data = StructureData(structures)
-        self.assertEqual(data.num, 25)
-        
-if __name__ == "__main__":
-    unittest.main()
+def test_structure_data(test_setup):
+    """测试StructureData基本功能"""
+    _, _, test_dir = test_setup
+    structures = Structure.read_multiple(os.path.join(test_dir, "data/nep/train.xyz"))
+    data = StructureData(structures)
+    assert data.num == 25

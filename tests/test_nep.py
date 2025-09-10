fab898745cc849bb00a9af887c26ff61943648f1
SuperBing <1747193328@qq.com>
2025-09-10 13:54:39 +0800

    修复测试文件

--- tests/test_nep.py
+++ tests/test_nep.py
@@ -11,7 +11,7 @@ from PySide6.QtWidgets import QApplication
 
 app = QApplication()
 Config()
-
+Config.set("nep", "backend","cpu")
 
 class TestNepTrainResultData( unittest.TestCase):
     def setUp(self):
